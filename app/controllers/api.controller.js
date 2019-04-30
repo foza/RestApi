b579a8535648aa5a90e8cd48fc5975f1878885d5
Fozil Umarov <mr.foza@bk.ru>
2019-04-30 12:32:50 +0500

    change create

--- app/controllers/api.controller.js
+++ app/controllers/api.controller.js
@@ -2,19 +2,34 @@ const db = require('../config/db.config.js');
 const UserDetail = db.UserDetail;
 
 exports.create = (req, res) => {
-    const id = req.params.id;
+    const userId = req.body.user_id,
+        typeR = req.body.type_r,
+        type = req.body.type,
+        amount = req.body.amount,
+        email = req.body.email,
+        order_id = req.body.order_id,
+        user_type = req.body.user_type;
+
     UserDetail.create(
         {
-            attributes: [
-                'created_at',
-                'type_r',
-                'amount',
-                'type']
-        },
-        {
-            where: {user_id: id}
-
+            user_id: userId,
+            type_r: typeR,
+            type: type,
+            amount: amount,
+            email: email,
+            order_id: order_id,
+            user_type: user_type
         }).then(userDetail => {
         res.send(userDetail);
     })
 };
+
+function isIdUnique(id) {
+    return db.Users.count({where: {id: id}})
+        .then(count => {
+            if (count != 0) {
+                return false;
+            }
+            return true;
+        });
+}
